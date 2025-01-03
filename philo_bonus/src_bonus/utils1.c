0abd5785441f55f1415af14c96453b919e3b9515
Adya Zahila Hasman Binti Aidi Hasman <abinti-a@u90z05s03.42kl.edu.my>
2025-01-03 18:16:06 +0800

    only close if the sem didn't fail

--- philo_bonus/src_bonus/utils1.c
+++ philo_bonus/src_bonus/utils1.c
@@ -6,7 +6,7 @@
 /*   By: abinti-a <abinti-a@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/24 13:27:39 by abinti-a          #+#    #+#             */
-/*   Updated: 2025/01/03 18:10:47 by abinti-a         ###   ########.fr       */
+/*   Updated: 2025/01/03 18:15:12 by abinti-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,17 +59,17 @@ void	handle_single_philo(t_philo *philo)
 
 void	cleanup(t_data *data)
 {
-	if (data->sem->forks)
+	if (data->sem->forks != SEM_FAILED)
 		sem_close(data->sem->forks);
-	if (data->sem->print_lock)
+	if (data->sem->print_lock != SEM_FAILED)
 		sem_close(data->sem->print_lock);
-	if (data->sem->stop_simulation)
+	if (data->sem->stop_simulation != SEM_FAILED)
 		sem_close(data->sem->stop_simulation);
-	if (data->sem->eating_lock)
+	if (data->sem->eating_lock != SEM_FAILED)
 		sem_close(data->sem->eating_lock);
-	if (data->sem->meal_count_lock)
+	if (data->sem->meal_count_lock != SEM_FAILED)
 		sem_close(data->sem->meal_count_lock);
-	if (data->sem->pid_lock)
+	if (data->sem->pid_lock != SEM_FAILED)
 		sem_close(data->sem->pid_lock);
 	sem_unlink("/forks");
 	sem_unlink("/print_lock");
